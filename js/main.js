83752eea508748812587803406070a21eda375d0
a-wotton <a_wotton@fanshaweonline.ca>
2024-02-14 12:00:01 -0500

    updated video player, and began changes on about

--- js/main.js
+++ js/main.js
@@ -27,15 +27,20 @@ function toggleMenu() {
 hamMenu.addEventListener("click", toggleMenu);
 
 //Greensock for about page
+let desc1 = document.querySelector("#desc1");
+let desc2 = document.querySelector(".desc2");
+
 const abouts = document.querySelectorAll(".about");
 abouts.forEach(about => {
 gsap.from(about, {
-    y: -200,
     autoAlpha: 0,
     scrollTrigger: {
         trigger: about,
+        // markers: true,
         scrub: true,
-        end: "+=1000"
+        pin: true,
+        start: "center center",
+        end: "+=200"
     }
 })
 })
@@ -45,10 +50,11 @@ const player = document.querySelector("video");
 const vidCon = document.querySelector("#vidcon");
 const videoControls = document.querySelector("#controls");
 const playButton = document.querySelector("#play-button");
+const playButtonIcon = document.querySelector("#play-button-icon");
 const pauseButton = document.querySelector("#pause-button");
 const stopButton = document.querySelector("#stop-button");
 const volumeSlider = document.querySelector("#change-vol");
-const fullScreen = document.querySelector("#full-screen")
+const fullScreen = document.querySelector("#full-screen");
 
 
 //remove default controls
@@ -56,12 +62,21 @@ player.controls = false
 videoControls.classList.remove("hidden");
 
 function playVideo() {
+    if (playButtonIcon.classList.contains("fa-play")) {
     player.play();
+    playButtonIcon.classList.remove("fa-play");
+    playButtonIcon.classList.add("fa-pause");
+ } else {
+    player.pause();
+    playButtonIcon.classList.remove("fa-pause");
+    playButtonIcon.classList.add("fa-play");
+ }
 }
 
-function pauseVideo() {
-    player.pause();
-}
+
+// function pauseVideo() {
+//     player.pause();
+// }
 
 function stopVideo() {
     player.pause();
@@ -75,17 +90,17 @@ function changeVolume() {
 
 function toggleFullScreen() {
     console.log("enter fullscreen")
-    if (!vidCon.fullScreenElement) {
+    if (!document.fullscreenElement) {
     vidCon.requestFullscreen();
-    } else if (vidCon.exitFullscreen){
-        vidCon.exitFullScreen()
+    } else {
+        document.exitFullscreen();
     }
 }
 //https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API (Source for fullscreen) 
 
 
 playButton.addEventListener("click", playVideo);
-pauseButton.addEventListener("click", pauseVideo);
+// pauseButton.addEventListener("click", pauseVideo);
 stopButton.addEventListener("click", stopVideo);
 volumeSlider.addEventListener("change", changeVolume);
 fullScreen.addEventListener("click", toggleFullScreen);
