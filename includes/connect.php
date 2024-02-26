671e21135f84e96a7360e19342964def00ac5186
a-wotton <a_wotton@fanshaweonline.ca>
2024-02-26 11:40:46 -0500

    made changes for web hosting

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=portfolio_db;charset=utf8mb4";
+$dsn = "mysql:host=xhrnyo07_portfolio;charset=utf8mb4";
 try {
-$connection = new PDO($dsn, 'root', 'root');
+$connection = new PDO($dsn, 'xhrnyo07_a_wotton', '86[iGWEhX%bW');
 } catch (Exception $e) {
   error_log($e->getMessage());
   exit('unable to connect');
